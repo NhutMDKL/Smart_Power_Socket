4d61fd2948fcf471b494377765b95387bccdc0bd
Nguyen Minh Nhut <nguyenminhnhut01072003@gmail.com>
2026-01-21 21:10:27 +0700

    Update TimeService.h

--- src/TimeService.h
+++ src/TimeService.h
@@ -2,6 +2,6 @@
 
 #include <Arduino.h>
 
-void timeInit();        // Initialize NTP and ESP32 system time
-void timeLoop();        // Update sysem time every 6 hours
-struct tm getNow();     // Get current time from ESP32 system time
+void timeInit();                // Initialize NTP and ESP32 system time
+void timeLoop();                // Update sysem time every 6 hours
+bool getNow(struct tm &out);   // Get current time from ESP32 system time
